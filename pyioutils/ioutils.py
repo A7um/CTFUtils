3a700788923db55a3973fcd90487be056ae53307
Atum <lgcpku@gmail.com>
2022-03-21 13:12:43 +0800

    pyioutils: add support for python3

--- pyioutils/ioutils.py
+++ pyioutils/ioutils.py
@@ -5,18 +5,28 @@ import struct
 import binascii
 import base64
 
-version = 1.1
+version = 1.2
 '''
 this is a self defined class as a simple pwntool replacement
 when there is not intalled pwntool 
 '''
+
+def _to_bytes(data,encoding = "latin-1"):
+    if sys.version_info.major==3 and type(data)==str :
+        print("BytesWarning: Text is not bytes; assuming ASCII, no guarantees.")
+        data = data.encode(encoding)
+    return data
+
 def u64(data):
+    data = _to_bytes(data)
     return struct.unpack("<Q",data)[0]
 
 def u32(data):
+    data = _to_bytes(data)
     return struct.unpack("<I",data)[0]
 
 def u16(data):
+    data = _to_bytes(data)
     return struct.unpack("<H",data)[0]
 
 def p64(data):
@@ -29,12 +39,15 @@ def p16(data):
     return struct.pack("<H",data)
 
 def u64l(data):
+    data = _to_bytes(data)
     return struct.unpack(">Q",data)[0]
 
 def u32l(data):
+    data = _to_bytes(data)
     return struct.unpack(">I",data)[0]
 
 def u16l(data):
+    data = _to_bytes(data)
     return struct.unpack(">H",data)[0]
 
 def p64l(data):
@@ -58,60 +71,70 @@ def a2h(data):
 def h2a(data):
     return binascii.unhexlify(data)
 
+
 class socket_io(object):
-    def sendline(self, delims):
-        return self.sock.send(delims + '\n')
+
+    def sendline(self, data):
+        data = _to_bytes(data)
+        return self.sock.send(data+b"\n")
  
-    def send(self, delims):
-        return self.sock.send(delims)
+    def send(self, data):
+        data = _to_bytes(data)
+        return self.sock.send(data)
      
     def recv(self, count):
         return self.sock.recv(count)
 
     def recvuntil(self, delims):
-        buf = ''
+        buf = b''
         while delims not in buf:
             buf += self.recv(1)
         return buf
  
     def recvline(self):
-        return self.recvuntil("\n")
+        return self.recvuntil(b"\n")
  
     def recvline_startswith(self, delims):
-        buf = ''
-        while '\n' + delims not in buf:
+        buf = b''
+        while b'\n' + delims not in buf:
             buf += self.recv(1)
          
         while True:
             tmp = self.recv(1)
             buf += tmp
-            if buf == '\n':
+            if buf == b'\n':
                 break
         return buf
     def close(self):
         self.sock.close()
 
     def interactive(self):
-        print 'Switching to interative mode'
+        print ('Switching to interative mode')
         go = threading.Event()
         def recv_thread():
             while not go.isSet():
                 try:
                     cur = self.recv(1)
-                    sys.stdout.write(cur)
+                    if sys.version_info.major==3:
+                        sys.stdout.buffer.write(cur)
+                    else:
+                        sys.stdout.write(cur)
                     sys.stdout.flush()
                 except EOFError:
-                    print 'Got EOF while reading in interactive'
+                    print ('Got EOF while reading in interactive')
                     break
         t = threading.Thread(target = recv_thread)
         t.setDaemon(True)
         t.start()
         while self.sock:
-            print '$ ',
+            print ('$ '),
             while True:
-                data = sys.stdin.read(1)
+                if sys.version_info.major==3:
+                    data = sys.stdin.buffer.read(1)
+                else:
+                    data = sys.stdin.read(1)
                 self.send(data)
-                if data == '\n':
+                if data == b'\n':
                     break
 
 class remote(socket_io):
@@ -121,13 +144,17 @@ class remote(socket_io):
         
     
 class process:
+
     def __init__(self, cmd):
         self.pipe = subprocess.Popen(cmd, stdin = subprocess.PIPE, stdout = subprocess.PIPE)
-    def sendline(self, delims):
-        return self.pipe.stdin.write(delims + '\n')
+
+    def sendline(self, data):
+        data = _to_bytes(data)
+        return self.pipe.stdin.write(data + b'\n')
  
-    def send(self, delims):
-        return self.pipe.stdin.write(delims)
+    def send(self, data):
+        data = _to_bytes(data)
+        return self.pipe.stdin.write(data)
      
     def recv(self, count):
         return self.pipe.stdout.read(count)
@@ -136,20 +163,20 @@ class process:
         return self.pipe.stdout.readline()
  
     def recvuntil(self, delims):
-        buf = ''
+        buf = b''
         while delims not in buf:
             buf += self.recv(1)
         return buf
 
     def recvline_startswith(self, delims):
-        buf = ''
-        while '\n' + delims not in buf:
+        buf = b''
+        while b'\n' + delims not in buf:
             buf += self.recv(1)
          
         while True:
             tmp = self.recv(1)
             buf += tmp
-            if buf == '\n':
+            if buf == b'\n':
                 break
         return buf
 
@@ -165,33 +192,39 @@ class process:
                 try:
                     cur = self.recv(1)
                 except EOFError:
-                    print 'Got EOF while reading in interactive'
+                    print ('Got EOF while reading in interactive')
                     break
         t = threading.Thread(target = recv_thread)
         t.setDaemon(True)
         t.start()
 
     def interactive(self):
-        print 'Switching to interative mode'
+        print ('Switching to interative mode')
         go = threading.Event()
         def recv_thread():
             while not go.isSet():
                 try:
                     cur = self.recv(1)
-                    sys.stdout.write(cur)
+                    if sys.version_info.major==3:
+                        sys.stdout.buffer.write(cur)
+                    else:
+                        sys.stdout.write(cur)                    
                     sys.stdout.flush()
                 except EOFError:
-                    print 'Got EOF while reading in interactive'
+                    print ('Got EOF while reading in interactive')
                     break
         t = threading.Thread(target = recv_thread)
         t.setDaemon(True)
         t.start()
         while self.pipe:
-            print '$ ',
+            print ('$ '),
             while True:
-                data = sys.stdin.read(1)
+                if sys.version_info.major==3:
+                    data = sys.stdin.buffer.read(1)
+                else:
+                    data = sys.stdin.read(1)
                 self.send(data)
-                if data == '\n':
+                if data == b'\n':
                     break
 
 class conn(socket_io):
@@ -208,4 +241,3 @@ class server:
     def get_conn(self):
         sock,addr=self.sock.accept()
         return conn(sock,addr)
-
